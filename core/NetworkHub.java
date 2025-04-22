ee816d875f2df7ef948dad492f44a2e7a6bd45fb
Shamura Ahmad <47661479+animshamura@users.noreply.github.com>
2025-04-22 13:26:52 +0600

    Update NetworkHub.java

--- core/NetworkHub.java
+++ core/NetworkHub.java
@@ -1,1 +1,24 @@
-// NetworkHub class here
+
+package core;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NetworkHub {
+    private List<Device> devices = new ArrayList<>();
+
+    public void connect(Device device) {
+        devices.add(device);
+    }
+
+    public List<Device> getAllDevices() {
+        return devices;
+    }
+
+    public Device getDeviceByName(String name) {
+        for (Device d : devices) {
+            if (d.getName().equals(name)) return d;
+        }
+        return null;
+    }
+}
