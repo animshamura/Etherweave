aae47e87ff392d655efcdd72e82290f2a3b9c244
Shamura Ahmad <47661479+animshamura@users.noreply.github.com>
2025-04-22 13:28:49 +0600

    Update DeviceServer.java

--- net/DeviceServer.java
+++ net/DeviceServer.java
@@ -1,1 +1,36 @@
-// DeviceServer class here
+
+package net;
+
+import core.Device;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class DeviceServer extends Thread {
+    private Device device;
+
+    public DeviceServer(Device device) {
+        this.device = device;
+    }
+
+    @Override
+    public void run() {
+        try (ServerSocket serverSocket = new ServerSocket(device.getPort())) {
+            System.out.println(device.getName() + " listening on port " + device.getPort());
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                String line;
+                while ((line = in.readLine()) != null) {
+                    System.out.println("[" + device.getName() + " RECEIVED]: " + line);
+                }
+                in.close();
+                clientSocket.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
